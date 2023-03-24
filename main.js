16b86ed4202293b5ddc1dca49e96ec1cfcf59a64
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-03-24 16:40:50 +0530

    add js-dom-using-dynamic-increment-and-decrement-input-values

---
+++ main.js
@@ -0,0 +1,32 @@
+let containerDiv = document.querySelector(".container");
+let btn = document.createElement("button");
+btn.className = "button";
+btn.innerText = "+";
+containerDiv.appendChild(btn);
+
+let input = document.createElement("input");
+input.className = "values";
+input.type = "text";
+input.value = "0";
+containerDiv.appendChild(input);
+
+let btn1 = document.createElement("button");
+btn1.className = "button";
+btn1.innerText = "-";
+containerDiv.appendChild(btn1);
+
+btn.addEventListener("click", increment);
+function increment() {
+    let count = document.querySelector(".values").value;
+    count++;
+    console.log(count);
+    document.querySelector(".values").value = count;
+}
+
+btn1.addEventListener("click", decrement);
+function decrement() {
+    let count = document.querySelector(".values").value;
+    count--;
+    console.log(count);
+    document.querySelector(".values").value = count;
+}
